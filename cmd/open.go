844d095a42db4186be582a7ede832ef0f5eb279b
Revi Studio's Owner <revistudios732@gmail.com>
2026-02-25 15:02:37 +0100

    Add support for opening projects in specific apps
    Allow users to specify the target app (finder, terminal, zed) using
    the --app flag. Default to "finder" if no target is provided.

--- cmd/open.go
+++ cmd/open.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
 
@@ -8,14 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var OpenCmd = &cobra.Command{
+var openCmd = &cobra.Command{
 	Use:   "open <project-name>",
 	Args:  cobra.ExactArgs(1),
 	Short: "Open a project",
 	Long:  "Open a project",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		config, err := lib.GetConfig()
-
 		if err != nil {
 			return fmt.Errorf("getting the config: %w", err)
 		}
@@ -23,11 +23,20 @@ var OpenCmd = &cobra.Command{
 		name := args[0]
 		path := config.ProjectFolderPath + "/" + name + "/"
 
-		// err := exec.Command("osascript", "-e", fmt.Sprintf(`tell application "Terminal" to do script "cd '%s'; clear"`, path)).Run()
+		target, _ := cmd.Flags().GetString("app")
 
-		err = exec.Command("open", path).Run()
+		switch target {
+		case "finder":
+			err = fmt.Errorf("finder: %w", exec.Command("open", path).Run())
+		case "terminal":
+			err = fmt.Errorf("terminal: %w", exec.Command("open", "-a", "Terminal", path).Run())
+		case "zed":
+			err = fmt.Errorf("zed: %w", exec.Command("zed", path).Run())
+		default:
+			return fmt.Errorf("unknown target: %s", target)
+		}
 
-		if err != nil {
+		if errors.Unwrap(err) != nil {
 			return fmt.Errorf("opening project %s: %w", name, err)
 		}
 		fmt.Printf("Opened project: %s\n", name)
@@ -37,5 +46,6 @@ var OpenCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(OpenCmd)
+	openCmd.Flags().StringP("app", "a", "finder", "--app <platform/target>")
+	rootCmd.AddCommand(openCmd)
 }
