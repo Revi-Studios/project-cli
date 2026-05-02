89650cee608b96fc6e9c323dd46e8bdbf33eb70c
Revi Studio's Owner <revistudios732@gmail.com>
2026-05-03 01:17:18 +0200

    Refactor config handling to use pointers and add new fields

--- lib/config.go
+++ lib/config.go
@@ -8,36 +8,55 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
-var (
-	ConfigPath = getConfigPath()
-)
+var ConfigPath = getConfigPath()
 
 type Config struct {
-	ProjectFolderPath string `toml:"config_path"`
+	Debug    bool     `toml:"debug"`
+	Editor   string   `toml:"editor"`
+	Projects Projects `toml:"projects"`
+	Defaults Defaults `toml:"defaults"`
+}
+
+type Projects struct {
+	Path      string `toml:"path"`
+	DebugPath string `toml:"debug_path"`
+}
+
+type Defaults struct {
+	Tag  string `toml:"tag"`
+	View string `toml:"view"`
+	Open string `toml:"open"`
+}
+
+func (this *Config) ProjectPath() string {
+	if this.Debug {
+		return this.Projects.DebugPath
+	}
+	return this.Projects.Path
 }
 
 // Loads and returns the config from the config file
-func GetConfig() (Config, error) {
+func GetConfig() (*Config, error) {
 	if err := validateConfigFile(); err != nil {
-		return Config{}, fmt.Errorf("validating the config: %w", err)
+		return nil, fmt.Errorf("validating the config: %w", err)
 	}
-	var config Config
+	var config *Config
 
 	if _, err := toml.DecodeFile(ConfigPath, &config); err != nil {
-		return Config{}, fmt.Errorf("reading config file: %w", err)
+		return nil, fmt.Errorf("reading config file: %w", err)
 	}
 	return config, nil
 }
 
 // Saves the config to the config file
-func SaveConfig(config Config) error {
+func SaveConfig(config *Config) error {
 	validateConfigFile()
 	conf, err := os.OpenFile(ConfigPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
 	defer conf.Close()
-	return toml.NewEncoder(conf).Encode(config)
+	return toml.NewEncoder(conf).Encode(*config)
 }
 
 // Makes sure a config file is present
@@ -60,6 +79,7 @@ func validateConfigFile() error {
 // Returns the correct path to the `config.toml` file by checking environment variables
 func getConfigPath() string {
 	switch true {
+
 	case os.Getenv("DEVELOPMENT") == "true" || os.Getenv("DEV") == "true":
 		return "./config.toml"
 	case os.Getenv("CONFIG_PATH") != "":
